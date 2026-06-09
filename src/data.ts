1b751e3d3ae693e8521bf989bad67bd81880c06e
WilsonMSouza <153249221+WilsonMSouza@users.noreply.github.com>
2026-06-08 21:43:12 -0300

    style: update product images
    Replace existing product images with new versions containing embedded C2PA metadata and updated SVG icons.

--- src/data.ts
+++ src/data.ts
@@ -1,8 +1,14 @@
 import { Product, AddonItem } from "./types";
 // @ts-ignore
-import cestaAmanhecerImg from "./assets/images/Cesta_Amanhecer_(final).png";
+import cestaAmanhecerImg from "./assets/images/cesta_amanhecer_(final).png";
 // @ts-ignore
-import cestaAmanhecerPremiumImg from "./assets/images/Cesta_Amanhecer_premium_(final).png";
+import cestaAmanhecerPremiumImg from "./assets/images/cesta_amanhecer_premium_(final).png";
+// @ts-ignore
+import cestaColonialImg from "./assets/images/cesta_colonial_(final).png";
+// @ts-ignore
+import cestaKidsImg from "./assets/images/cesta_kids_(final).png";
+// @ts-ignore
+import cestaRomanticaImg from "./assets/images/cesta_romantica_(final).png";
 
 export const PRODUCTS: Product[] = [
   {
@@ -19,15 +25,15 @@ export const PRODUCTS: Product[] = [
         price: 152.00,
         description: "Ideal para servir 1 pessoa com requinte e doçura na medida certa.",
         items: [
-          "Mini Bolo Artesanal de Laranja",
-          "Croissant Amanteigado Folhado",
-          "Pão de Queijo, Bolacha Artesanal, Torradinhas e Stroopwafel",
-          "Frios Especiais Fatiados: queijo minas, queijo muçarela, salame italiano e lombo canadense",
-          "Geleia Artesanal e Manteiga",
-          "Bombom Artesanal e Frutas da Época",
-          "Suco de Uva Integral (250 a 300 ml)",
-          "Drip Coffee Especial Gourmet",
-          "Sache de Capuccino e Sache de Chá",
+          "Mini bolo artesanal de laranja",
+          "Croissant amanteigado",
+          "Pão de queijo, bolacha artesanal, torradinhas e stroopwafel",
+          "Frios especiais fatiados: queijo minas, queijo muçarela, salame italiano e lombo canadense",
+          "Geleia artesanal e manteiga",
+          "Bombom artesanal e frutas da época",
+          "Suco de uva integral (250 a 300 ml)",
+          "Drip coffee especial gourmet",
+          "Sache de capuccino e sache de chá",
           "Opção de Embalagem 1: Caixa de MDF, 20X20cm, embalada em celofane e finalizada com laço de cetim (acompanha cartão de mensagem).",
           "Opção de Embalagem 2: Sacola fosca de alça redonda, 27x23cm, finalizada com laço de cetim (acompanha cartão de mensagem)."
         ] 
@@ -38,15 +44,15 @@ export const PRODUCTS: Product[] = [
         price: 227.00,
         description: "Perfeito para compartilhar (serve 2 pessoas).",
         items: [
-          "Mini Bolo Artesanal de Laranja",
-          "Croissant Amanteigado Folhado",
-          "Pão de Queijo, Bolacha Artesanal, Torradinhas e Stroopwafel",
-          "Frios Especiais Fatiados: queijo minas, queijo muçarela, salame italiano e lombo canadense",
-          "Geleia Artesanal e Manteiga",
-          "Bombom Artesanal e Frutas da Época",
-          "Suco de Uva Integral (250 a 300 ml)",
-          "Drip Coffee Especial Gourmet",
-          "Sache de Capuccino e Sache de Chá",
+          "Mini bolo artesanal de laranja",
+          "Croissant amanteigado",
+          "Pão de queijo, bolacha artesanal, torradinhas e stroopwafel",
+          "Frios especiais: queijo minas, queijo muçarela, salame italiano e lombo canadense",
+          "Geleia artesanal e manteiga",
+          "Bombom artesanal e frutas da época",
+          "Suco de uva integral (250 a 300 ml)",
+          "Drip coffee especial gourmet",
+          "Sache de capuccino e sache de chá",
           "Opção de Embalagem 1: Embalagem redonda, 27cm, em MDF, embalada em celofane e finalizada com laço de cetim (acompanha cartão de mensagem).",
           "Opção de Embalagem 2: Sacola fosca de alça redonda, 27x23cm, finalizada com laço de cetim (acompanha cartão de mensagem)."
         ]
@@ -56,7 +62,7 @@ export const PRODUCTS: Product[] = [
   {
     id: "amanhecer-premium",
     name: "Amanhecer Premium",
-    tagline: "Sabor refinado de micro lotes",
+    tagline: "O sabor refinado que encanta",
     description: "Nossa consagrada seleção de croissants amanteigados folhados, frios especiais fatiados, geleia artesanal, drip coffee especial gourmet e muito mais.",
     image: cestaAmanhecerPremiumImg,
     sizes: [
@@ -66,15 +72,15 @@ export const PRODUCTS: Product[] = [
         price: 285.50,
         description: "Nossa seleção mais luxuosa com itens premium (serve 2 a 3 pessoas).",
         items: [
-          "Bolo Artesanal de Laranja",
-          "Croissant Amanteigado Folhado",
-          "Pão de Queijo com Catupiry, Grissini, Sfogliatini e Torradinhas",
-          "Frios Especiais Fatiados: queijo minas, queijo muçarela, salame italiano e lombo canadense",
-          "Geleia Artesanal, Manteiga e Mel",
-          "Granola, Damasco, Castanha, Bombom Ferrero Rocher e Frutas da Época",
-          "Suco de Uva Integral (500 ml)",
-          "Drip Coffee Especial Gourmet",
-          "Sache de Capuccino e Sache de Chá",
+          "Bolo artesanal de laranja",
+          "Croissant amanteigado",
+          "Pão de queijo com catupiry, grissini, sfogliatini e torradinhas",
+          "Frios especiais: queijo minas, queijo muçarela, salame italiano e lombo canadense",
+          "Geleia artesanal, manteiga e mel",
+          "Granola, damasco, castanha, bombom Ferrero Rocher e frutas da época",
+          "Suco de uva integral (500 ml)",
+          "Drip coffee especial gourmet",
+          "Sache de capuccino e sache de chá",
           "Embalagem: Caixa circular em MDF, de 27cm, ou caixa em formato de coração em MDF, de 30 cm, embalada em celofane e finalizada com laço de cetim; acompanha cartão de mensagem."
         ]
       }
@@ -84,37 +90,43 @@ export const PRODUCTS: Product[] = [
     id: "cesta-colonial",
     name: "Cesta Colonial",
     tagline: "O aconchego rústico do campo",
-    description: "Pão artesanal rústico de fermentação natural, queijo colonial regional, salame italiano fatiado, bolo de fubá quentinho com erva-doce, geleia fresca caseira e biscoitos amanteigados.",
-    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuAFZSQuhjkEmPnKlTmBfWoTO1PVDXY5CfqFZn6BVN5pnXtgzLPN2shjr3KZ_He2RiSh7-xa65TsOuxwgYLJalsHPaxlaJwcAFlReXmKz8OfVTRzdlmZ_Zkg1O438p_6yHTEyh3Ba5C-dD1OtQENlQNgesoTVCMUlvvNQ5lWkMmCiIaP57VGbUk8G70rfZMLg-tag3_V2oo8pija14eRjKjkCLVQ5JQeh_hAjzYDFZ68WIQiRDifZqkwyr_u0B3-Hk9pDwr2JjqQ_TE",
+    description: "Bolo de laranja, broa de milho, pão artesanal, salame e queijo coloniais, geléia artesanal, drip coffee e muito mais.",
+    image: cestaColonialImg,
     sizes: [
       { 
         label: "Tamanho M", 
         shortLabel: "Tam. M", 
         price: 225.00,
-        description: "Sabores rústicos afetivos coloniais que resgatam o melhor da fazenda.",
+        description: "Sabores rústicos coloniais que resgatam o melhor da fazenda (serve 2 pessoas).",
         items: [
-          "1 Pão Italiano Rústico de Fermentação Natural",
-          "Queijo Colonial Curado Regional (150g)",
-          "Salame Italiano fatiado fininho (80g)",
-          "Bolo de Fubá Fresquinho com toque de erva-doce",
-          "Geleia caseira de Uva ou Amora (120g)",
-          "Biscoitos Amanteigados tradicionais crocantes (100g)"
+          "Bolo de laranja ou cenoura",
+          "Broa de milho ou cuca",
+          "Bolacha artesanal, Pão artesanal e Pão de queijo",
+          "Salame e queijo coloniais",
+          "Geléia artesanal, patê de ricota, manteiga e mel",
+          "Frutas da época e bombom artesanal",
+          "Suco de uva integral (500 ml)",
+          "Drip coffee (café para coar)",
+          "Sache de Capuccino e Sache de Chá",
+          "Embalagem: Cesta de vime, redonda, 25x7cm, finalizada com fita de juta natural; acompanha cartão de mensagem e guardanapos."
         ]
       },
       { 
         label: "Tamanho G", 
         shortLabel: "Tam. G", 
         price: 260.00,
-        description: "Versão farta e abundante com queijos e charcutaria colonial reforçada.",
+        description: "Versão farta com os sabores da fazenda (serve 3 a 4 pessoas).",
         items: [
-          "1 Pão Colonial Grande artesanal",
-          "Pão de Queijo da Casa assado no dia (4 unidades)",
-          "Cunha de Queijo Colonial Curado (250g)",
-          "Salame Italiano Fatiado & Copa Curada (150g)",
-          "Bolo de Fubá com Erva-doce Inteiro",
-          "2 Geleias Frescas Caseiras Sortidas",
-          "Manteiga Colonial da Região",
-          "Biscoitos Amanteigados Rústicos em dobro (200g)"
+          "Bolo de laranja ou cenoura",
+          "Broa de milho ou cuca",
+          "Bolacha artesanal, Pão artesanal e Pão de queijo",
+          "Salame e queijo coloniais",
+          "Geléia artesanal, patê de ricota, manteiga e mel",
+          "Frutas da época e bombom artesanal",
+          "Suco de uva integral (500 ml)",
+          "Drip coffee (café para coar)",
+          "Sache de Capuccino e Sache de Chá",
+          "Embalagem: Cesta de vime,  redonda, 30x7cm, finalizada com fita de juta natural; acompanha cartão de mensagem."
         ]
       }
     ]
@@ -123,24 +135,25 @@ export const PRODUCTS: Product[] = [
     id: "cesta-romantica",
     name: "Cesta Romântica",
     tagline: "Eternize histórias de amor",
-    description: "Espetacular caixa em mdf com formato de coração, repleta de rosas vermelhas importadas selecionadas, bombons belgas de avelã finos e itens exclusivos para presentear a pessoa amada.",
-    image: "https://lh3.googleusercontent.com/aida-public/AB6AXuAB_cWRvSXPiV1egbm-aG2HIKrwayiXa4ZetrvaIKCvg2mj6YmuVgdgw1vJom6omeFMoWiq9MxSvQQHbTGZIOtkV8reI4T8eZB1O0AeUrAUOS7v6t4ukSiwO8_pBwF_icgjpNXyPd17OvhzAGw4sZUelvtCbRG3Ovfcq83NOYel5A9FTmm40ZBapmdKyk7gD0eK6F33aQfzKmCScA2spsT9GznxVX7YDNKcaB6YY-YHlBrADQ2voXLWE1lvI_GlKCXKPiYUcnwhq24",
-    badge: "Especial Dia dos Namorados",
-    isSpecial: true,
+    description: "Pão de ló, croissant amanteigado, cupcake, Stropp waffle, bolachas artesanais, frios especiais, drip coffee e muito mais...",
+    image: cestaRomanticaImg,
     sizes: [
       { 
-        label: "Tamanho M Único", 
+        label: "Tamanho Único", 
         shortLabel: "R$ 240,00", 
         price: 240.00,
-        description: "A joia da nossa coleção: uma inesquecível celebração da paixão e do afeto.",
+        description: "Uma inesquecível celebração da paixão e do afeto (serve 2 pessoas).",
         items: [
-          "Caixa de Luxo em MDF Rústico em formato de Coração",
-          "6 Rosas Vermelhas Importadas Selecionadas",
-          "Bombons Belgas Gourmet de Avelã Gold Chocolatier",
-          "Morangos Frescos selecionados com calda de chocolate",
-          "1 Garrafa de Espumante Premium de uva (sem álcool)",
-          "Taça de Cristal Lapidada exclusiva",
-          "Cartão de Dedicatória Escrito à Mão em bico de pena"
+          "Pão de ló e croissant amanteigado",
+          "Cupcake e Stropp Waffle",
+          "Bolachas artesanais, pão de queijo, e torradinhas",
+          "Frios especiais: queijo minas, queijo muçarela, salame italiano e lombo canadense",
+          "Geléia artesanal e manteiga",
+          "Frutas da época e trufas",
+          "Suco de uva (long neck) gaseificado",
+          "Drip coffee (café para coar)",
+          "Sache de Capuccino e Sache de Chá",
+          "Embalagem: caixa de MDF, em formato de coração (30x7cm), embalada em celofane e finalizada com laço de cetim; acompanha cartão de mensagem."
         ]
       }
     ]
